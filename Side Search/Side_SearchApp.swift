3fe4acee7c5b494fb1ee0e1e38c7ead717b22935
Cizzuk <dropcat@cizzuk.net>
2025-12-24 13:58:58 +0900

    Create Project

---
+++ Side Search/Side_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Side_SearchApp.swift
+//  Side Search
+//
+//  Created by Cizzuk on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Side_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
