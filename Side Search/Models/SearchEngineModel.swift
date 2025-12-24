ed37146272a77285b5899f51f1693b55dd029a9b
Cizzuk <dropcat@cizzuk.net>
2025-12-24 14:41:55 +0900

    Create SearchEngine Data Model

---
+++ Side Search/Models/SearchEngineModel.swift
@@ -0,0 +1,29 @@
+//
+//  SearchEngineModel.swift
+//  Side Search
+//
+//  Created by Cizzuk on 2025/12/24.
+//
+
+import Foundation
+
+struct SearchEngineModel: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var url: String
+    var disablePercentEncoding: Bool = false
+    var maxQueryLength: Int? = nil
+}
+
+// Encode/Decode JSON
+extension SearchEngineModel {
+    func toJSON() -> Data? {
+        let encoder = JSONEncoder()
+        return try? encoder.encode(self)
+    }
+    
+    static func fromJSON(_ data: Data) -> SearchEngineModel? {
+        let decoder = JSONDecoder()
+        return try? decoder.decode(SearchEngineModel.self, from: data)
+    }
+}
