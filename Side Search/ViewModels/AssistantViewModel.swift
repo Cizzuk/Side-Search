379a2d2bc9409c0bbfd8bf034087fa5b2e6167d0
Cizzuk <dropcat@cizzuk.net>
2026-01-11 02:05:40 +0900

    Remove VoiceOver Alert

--- Side Search/ViewModels/AssistantViewModel.swift
+++ Side Search/ViewModels/AssistantViewModel.swift
@@ -21,28 +21,6 @@ class AssistantViewModel: ObservableObject {
     @Published var isCriticalError = false
     @Published var showError = false
     
-    // VoiceOver Alert
-    @Published var showVoiceOverAlert = false
-    private var dontShowVoiceOverAlert: Bool = UserDefaults.standard.bool(forKey: "dontShowVoiceOverAlert")
-    enum VoiceOverAlertAction {
-        case proceed
-        case dontShowAgain
-        case cancel
-    }
-    func handleVoiceOverAlertAction(_ action: VoiceOverAlertAction) {
-        switch action {
-        case .proceed:
-            dontShowVoiceOverAlert = true
-            startRecording()
-        case .dontShowAgain:
-            UserDefaults.standard.set(true, forKey: "dontShowVoiceOverAlert")
-            dontShowVoiceOverAlert = true
-            startRecording()
-        case .cancel:
-            break
-        }
-    }
-    
     // Get SearchEngine Settings
     @Published var SearchEngine: SearchEngineModel = {
         if let rawData = UserDefaults.standard.data(forKey: "defaultSearchEngine"),
@@ -109,13 +87,6 @@ class AssistantViewModel: ObservableObject {
             if recognitionTask != nil {
                 recognitionTask?.cancel()
                 recognitionTask = nil
-            }
-            
-            // Check VoiceOver
-            let isVoiceOverRunning = UIAccessibility.isVoiceOverRunning
-            if isVoiceOverRunning && !dontShowVoiceOverAlert {
-                showVoiceOverAlert = true
-                return
             }
             
             // Check Availability
