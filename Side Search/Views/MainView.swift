628b23bd89325c8ee83afd36419050756f4bf145
Cizzuk <dropcat@cizzuk.net>
2026-03-31 15:29:06 +0900

    Update MainView.swift

--- Side Search/Views/MainView.swift
+++ Side Search/Views/MainView.swift
@@ -65,7 +65,6 @@ struct MainView: View {
                     Text("URL Settings")
                 } footer: {
                     Text("If you select open in Default App, the app corresponding to the URL or the default browser will be opened.")
-                        .padding(.bottom, 10)
                 }
                 
                 // Background Settings
