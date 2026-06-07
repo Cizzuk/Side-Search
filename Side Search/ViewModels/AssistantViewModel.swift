764c77d7695d9a66b07ebfc7a95a89e970595e30
Cizzuk <dropcat@cizzuk.net>
2026-06-07 16:57:29 +0900

    Create AssistantViewModel.swift

---
+++ Side Search/ViewModels/AssistantViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AssistantViewModel.swift
+//  Side Search
+//
+//  Created by Cizzuk on 2026/06/07.
+//
+
+import Combine
+import SwiftUI
+
+class AssistantViewModel: ObservableObject {
+    
+}
